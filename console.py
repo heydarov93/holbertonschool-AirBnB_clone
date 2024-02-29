b0229c4d91e35e85b608c5d2d29913957187b69e
heydarov93 <heyderov.yashar@mail.ru>
2024-02-29 19:20:28 +0400

    task7: solved up to show (included), left destroy and below. Needs to create check_args function to check classname, id etc. to prevent repeating yourself

--- console.py
+++ console.py
@@ -6,6 +6,9 @@ command interpreter
 
 
 import cmd
+from json import load
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -13,14 +16,58 @@ class HBNBCommand(cmd.Cmd):
     Command interpreter
     """
     prompt = '(hbnb) '
+    __storage_filename = "file.json"
+    __base_classname = "BaseModel"
 
-    def do_quit(self, arg):
+    def do_create(self, args):
+        """
+        Creates a new instance of BaseModel,
+        saves it (to the JSON file) and prints the id
+        """
+        if args == "":
+            print("** class name missing **")
+        elif args != self.__base_classname:
+            print("** class doesn't exist **")
+        else:
+            instance = eval(f"{args}()")
+            instance.save()
+            print(instance.id)
+
+    def do_show(self, args):
+        """
+        Prints the string representation of an instance
+        based on the class name and id
+        """
+        clargs = args.split()
+        length = len(clargs)
+
+        if length == 0:
+            print("** class name missing **")
+        elif clargs[0] != self.__base_classname:
+            print("** class doesn't exist **")
+        elif length == 1:
+            print("** instance id missing **")
+        else:
+            classname = self.__base_classname
+            key = f"{self.__base_classname}.{clargs[1]}"
+            objs = storage.all()
+
+            if not objs or not objs.get(key):
+                print("** no instance found **")
+            else:
+                print(objs[key])
+
+    @staticmethod
+    def check_args(classname, id):
+        pass
+
+    def do_quit(self, args):
         """
         Quit command to exit the program
         """
         return True
 
-    def do_EOF(self, arg):
+    def do_EOF(self, args):
         """
         Exits the program
         """
@@ -28,6 +75,20 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         pass
+
+    """
+    def __reload_instances(self):
+        
+        Reloads objects from file and returns dictionary
+        
+        filename = self.__storage_filename
+        try:
+            with open(filename, "r", encoding="utf-8") as afile:
+                objs = load(afile)
+                return objs
+        except FileNotFoundError:
+            return False
+    """
 
 
 if __name__ == '__main__':
