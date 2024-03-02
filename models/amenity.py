2349f7d91ef4ad1e002717fe0c39247a589c90d4
heydarov93 <heyderov.yashar@mail.ru>
2024-03-02 20:45:31 +0400

    Amenity class created

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This is a 'amenity' module"""
+
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Amenity class that inherits from BaseModel
+    """
+
+    name = ""
