2a8755f30d33a0f75e9b4a51419ce456bda5496d
heydarov93 <heyderov.yashar@mail.ru>
2024-03-02 20:46:00 +0400

    Review class created

---
+++ models/review.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This is a 'review' module"""
+
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """
+    Review class that inherits from BaseModel
+    """
+
+    place_id = ""
+    user_id = ""
+    text = ""
